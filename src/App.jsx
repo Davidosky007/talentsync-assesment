e6677b932987d696f758428b779651be2227243b
David Bassey <davidosky1@gmail.com>
2024-01-29 16:54:24 +0100

    Add Edge and Partner components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import Edge from "./components/Edge";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Partner from "./components/Partner";
 function App() {
   return (
     <>
       <div className="container mx-auto p-16">
         <Navbar/>
         <Header/>
+        <Partner/>
+        <Edge/>
       </div>
     </>
   );
